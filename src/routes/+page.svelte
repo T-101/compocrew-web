70da37e8e2eeac5602a19cb8778ea94f47cec93d
T-101 <antti.rummukainen@gmail.com>
2025-03-04 15:39:28 +0200

    unify terms and appearance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <h2 class="mb-4">CompoCrew ry</h2>
 <p>Olemme voittoa tavoittelematon yhdistys, jolta voi anoa tukea suomalaisiin demoscene-tapahtumiin.</p>
 
-<p>CompoCrew Ry on perustettu Huhtikuussa 2021 edistämään Suomalaista tietokonekulttuuria, demosceneä. Yhdistys pyrkii
+<p>CompoCrew ry on perustettu Huhtikuussa 2021 edistämään Suomalaista tietokonekulttuuria, demosceneä. Yhdistys pyrkii
     tukemaan Suomalaisia demoscene-harrastajia ja yhdistyksiä tarjoamalla tietotaitoa, tekniikkaa sekä perustellusti
     rahoitusta tapahtumien järjestämiseen.</p>
 
 <p>Yhdistyksellä on tietokonetaidekilpailujen järjestämistä varten nykyaikainen tietokonelaitteisto,
-    videokaappauslaitteistoa sekä tarkoitusta varten kehitetty tietokonetaidekilpailujen hallintatyökalu Partyman.</p>
+    videokaappauslaitteistoa sekä tarkoitusta varten kehitetty tietokonetaidekilpailujen hallintatyökalu PartyMan.</p>
